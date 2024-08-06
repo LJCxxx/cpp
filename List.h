e1f47453858a7f741d0837733326848253018bb1
LJCxxx <105922644+LJCxxx@users.noreply.github.com>
2024-08-06 18:24:13 +0800

    Update List.h

--- List.h
+++ List.h
@@ -2,6 +2,19 @@
 #define CUSTOM_LIST_H
 #include <iostream>
 
+template<typename K, typename V>
+class Entry{
+private:
+	K _key;
+	V _value;
+public:
+	Entry(const K& k = K(), const V& v = V()): _key(k), _value(v) {}
+ 	const K& key() const {return _key;}
+ 	const V& value() const {return _value;}
+	void setKey(const K& k) {_key = k;}
+	void setValue(const V& v) {_value=v;}
+};
+
 template <typename T>
 class List{
 
