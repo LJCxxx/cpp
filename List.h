0c8ff66d0a8456a383057bd8652557c4545cc87b
LJCxxx <105922644+LJCxxx@users.noreply.github.com>
2024-08-06 18:35:25 +0800

    Update List.h

--- List.h
+++ List.h
@@ -5,14 +5,14 @@
 template<typename K, typename V>
 class Entry{
 private:
-	K _key;
-	V _value;
+  K _key;
+  V _value;
 public:
-	Entry(const K& k = K(), const V& v = V()): _key(k), _value(v) {}
- 	const K& key() const {return _key;}
- 	const V& value() const {return _value;}
-	void setKey(const K& k) {_key = k;}
-	void setValue(const V& v) {_value=v;}
+  Entry(const K& k = K(), const V& v = V()): _key(k), _value(v) {}
+  const K& key() const {return _key;}
+  const V& value() const {return _value;}
+  void setKey(const K& k) {_key = k;}
+  void setValue(const V& v) {_value=v;}
 };
 
 template <typename T>
