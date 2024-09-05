4d16867679248efcf65a6d46087584b5b8758609
Benjamin ⠀ <benlian23@gmail.com>
2024-09-05 14:51:28 +0800

    "added linalg folder"

---
+++ linalg/cholesky_decomp.cpp
@@ -0,0 +1,104 @@
+#include <concepts>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+template<class T>
+concept Numerical = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
+template <Numerical T>
+void cholesky_decompositon(const T* A, T* L, const size_t size) {
+  for(int i = 0; i < size; i++) {
+    for(int j = 0; j <= i; j++) {
+      if(i == j) {
+        auto tmp = A[size * i + j];
+        for(int k = 0; k < j; k++) {
+          tmp -= L[size * i + k] * L[size * i + k];
+        }
+        L[size * i + j] = std::sqrt(tmp);
+      } else {
+        auto tmp = A[size * i + j];
+        for(int k = 0; k < j; k++) {
+          tmp -= L[size * i + k] * L[size * (i - 1) + k];
+        }
+        L[size * i + j] = tmp / L[size * (j) + j];
+      }
+    }
+  }
+}
+
+template <Numerical T>
+void gemm(const T* A, const T* B, T* C, size_t rowA, size_t colA, size_t colB) {
+  for(size_t i = 0; i < rowA; i++) {
+    for(size_t j = 0; j < colB; j++) {
+      for(size_t k = 0; k < colA; k++) {
+        C[i * colB + j] += A[i * colA + k] * B[k * colB + j];
+      }
+    }
+  }
+};
+
+template <Numerical T>
+void transpose(const T* A, T* AT, size_t rowA, size_t colA) {
+  for(size_t i = 0; i < colA; i++) {
+    for(size_t j = 0; j < rowA; j++) {
+      AT[i * rowA + j] = A[j * colA + i];
+    }
+  }
+};
+
+void print(auto* A, size_t rowA, size_t colA) {
+  for(int i = 0; i < rowA * colA;  i++) {
+    std::cout<< A[i] << ((i+1)%rowA == 0 ? "\n" : ", ");
+  }
+} 
+
+int main() {
+  unsigned const int rowA = 3;
+  float A[rowA * rowA] {
+    4, 12, -16,
+    12, 37, -43,
+    -16, -43, 98
+  };
+
+  float L[rowA * rowA] {0.0};
+  float LT[rowA * rowA] {0.0};
+  float LLT[rowA * rowA] {0.0};
+
+  cholesky_decompositon(A, L, rowA);
+  transpose(L, LT, 3, 3);
+  gemm(L, LT, LLT, 3, 3, 3);
+  
+  std::cout<< "matrix A:\n";
+  print(A, rowA, rowA);
+
+  std::cout<< "\nmatrix L:\n";
+  print(L, rowA, rowA);
+
+  std::cout<< "\nmatrix LT:\n";
+  print(LT, rowA, rowA);
+
+  std::cout<< "\nmatrix LLT:\n";
+  print(LLT, rowA, rowA);
+}
+// prints
+// 
+// matrix A:
+// 4, 12, -16
+// 12, 37, -43
+// -16, -43, 98
+
+// matrix L:
+// 2, 0, 0
+// 6, 1, 0
+// -8, 5, 3
+
+// matrix LT:
+// 2, 6, -8
+// 0, 1, 5
+// 0, 0, 3
+
+// matrix LLT:
+// 4, 12, -16
+// 12, 37, -43
+// -16, -43, 98
