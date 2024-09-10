2e604345bcf9db4c4085e8dd94d009197596ca0d
LJCxxx <105922644+LJCxxx@users.noreply.github.com>
2024-09-10 21:08:24 +0800

    Update find_winners.cpp

--- examples/find_winners.cpp
+++ examples/find_winners.cpp
@@ -8,7 +8,7 @@
 //
 // While find_winners is a straightforward problem typically solvable with a loop,
 // a couple of conditions, and a counter, a more modular and functional approach
-// can be taken with c++11 variadic templates and c++20 ranges. The predicate function
+// can be taken with lambdas and c++20 ranges. The predicate function
 // can be composed with lambdas—one for the factor check and another for the
 // must-have digit. iota generates the 1 to n range. Then, the predicate function is 
 // applied by piping filter to iota. Finally, distance returns the number of winners.
@@ -17,42 +17,24 @@
 // This version emphasizes modularity by highlighting how each part of the solution
 // can be composed and reused.
 
+#include <string>
 #include <ranges>
-#include <string_view>
 #include <iostream>
 
-template <typename F, typename... G>
-auto compose(F&& f, G&&... g) {
-  return [=](auto&&... args) {
-    return f(compose(g...)(std::forward<decltype(args)>(args)...));
+auto find_winners(int f, int m, int n) {
+  auto factor = [f](auto e) { return e % f == 0; };
+  auto must_have = [m](auto e) {
+    return std::to_string(e).find(std::to_string(m)) != std::string::npos;
   };
-}
-
-template <typename F>
-auto compose(F&& f) {
-  return [=](auto&&... args) {
-    return f(std::forward<decltype(args)>(args)...);
+  auto predicate = [=](auto e) {
+    return factor(e) ? must_have(e) : false;
   };
-}
-
-auto find_winners(const int f, const int m, const int n) {
-
-  auto factor = [f](auto e) { return e % f == 0 ? e : 0; };
-  auto must_have = [m](auto e) { return e == 0 ? false : 
-    std::to_string(e).find(std::to_string(m)) != std::string::npos;
-  };
-  auto predicate = compose(must_have, factor);
-
-  // This variant simplifies composition by individually checking factor and
-  // must_have rather than chaining the range values through multiple lambdas.
-  // auto factor = [f](auto e) { return e % f == 0; };
-  // auto must_have = [m](auto e) { return std::to_string(e).find(std::to_string(m)) != std::string::npos; };
-  // auto predicate = [=](auto e) { return factor(e) && must_have(e); };
-
-  // remove std::ranges::distance to return list of winners
+  
+  // return (std::views::iota(1, n + 1) | std::views::filter(predicate));
   return std::ranges::distance(std::views::iota(1, n + 1) | std::views::filter(predicate));
 }
 
 int main() {
-  std::cout<<find_winners(3,5,100); //prints 6
+  // for(auto&& it : find_winners(3,5,100)) std::cout<<it<<'\n'; // prints 15 45 51 54 57 75
+  std::cout<<find_winners(3,5,100); // prints 6
 }
